6a0ed4195815b0c9dc8230a797925e400c3780ea
technolearn-cloud1 <154865724+technolearn-cloud1@users.noreply.github.com>
2024-01-03 10:07:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.get('/', function (req, res) {
   res.render('index', {
 
-    title: 'ElasticbeanStalk demo by technolearn-cloud1'
+    title: 'demo by technolearn-cloud1'
 
     })
     res.status(200).end();
