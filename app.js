1edf3309d2a5db96c2c566b7b4f0e15850d328df
technolearn-cloud1 <154865724+technolearn-cloud1@users.noreply.github.com>
2024-01-04 06:11:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.get('/', function (req, res) {
   res.render('index', {
 
-    title: 'technolearn-cloud1'
+    title: 'technolearn-devops'
 
     })
     res.status(200).end();
